22a7a2f53cbad7bfd15ab1446957ce30d0f29ca3
ptsypyshev <ptsypyshev@gmail.com>
2022-05-05 12:26:01 +0500

    Revert "Add CheckBulk feature"
    This reverts commit 1dcc8ae9162eae7b29db573be810990e3f6a5f15.

--- checker/checker.go
+++ checker/checker.go
@@ -1,34 +1,38 @@
 package checker
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
-//func CheckWebServer(urlStr string) {
-//	resp, err := http.Get(urlStr)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	fmt.Println("The status code we got is:", resp.StatusCode)
-//}
-
-func CheckWebServer(urlStr string) (code int, err error) {
+func CheckWebServer(urlStr string) {
 	resp, err := http.Get(urlStr)
 	if err != nil {
-		//log.Fatal(err)
-		return 0, err
+		log.Fatal(err)
 	}
-	return resp.StatusCode, nil
-	//fmt.Println("The status code we got is:", resp.StatusCode)
+	fmt.Println("The status code we got is:", resp.StatusCode)
 }
 
-func CheckBulk(listUrls []string) map[string]int {
-	result := make(map[string]int)
-	for _, u := range listUrls {
-		res, err := CheckWebServer(u)
-		if err != nil {
-			result[u] = 1000
-		} else {
-			result[u] = res
-		}
-	}
-	return result
-}
+//func CheckWebServer(urlStr string) (code int, err error) {
+//	resp, err := http.Get(urlStr)
+//	if err != nil {
+//		//log.Fatal(err)
+//		return 0, err
+//	}
+//	return resp.StatusCode, nil
+//	//fmt.Println("The status code we got is:", resp.StatusCode)
+//}
+//
+//func CheckBulk(listUrls []string) map[string]int {
+//	result := make(map[string]int)
+//	for _, u := range listUrls {
+//		res, err := CheckWebServer(u)
+//		if err != nil {
+//			result[u] = 1000
+//		} else {
+//			result[u] = res
+//		}
+//	}
+//	return result
+//}
